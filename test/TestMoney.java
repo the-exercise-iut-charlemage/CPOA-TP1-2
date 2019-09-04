8ebba64e3635abcfba61cdb07f479180edbea544
Safyrus <54881649+Safyrus@users.noreply.github.com>
2019-09-04 11:43:36 +0200

    correction

--- test/TestMoney.java
+++ test/TestMoney.java
@@ -12,34 +12,34 @@ public class TestMoney {
     @Test
     public void testMontant() {
         Money money = new Money(10, "EUR");
-        assertEquals("La valeur redonné et pas la bonne", 10, money.getMontant());
+        assertEquals("La valeur redonné n'est pas la bonne", 10, money.getMontant());
     }
 
     @Test
     public void testMontantAdd() {
         Money money = new Money(10, "EUR");
         money = money.add(money);
-        assertEquals("La valeur redonné et pas la bonne", 20, money.getMontant());
+        assertEquals("La valeur redonné n'est pas la bonne", 20, money.getMontant());
     }
 
     @Test
     public void testDevise() {
         Money money = new Money(10, "EUR");
-        assertEquals("La valeur redonné et pas la bonne", "EUR", money.getDevise());
+        assertEquals("La valeur redonné n'est pas la bonne", "EUR", money.getDevise());
     }
 
     @Test
-    public void testEqualite() {
+    public void testEgalite() {
         Money money = new Money(10, "EUR");
-        assertFalse("Un string n'est a un Money", money.equals("Hello"));
-        assertTrue("Money doit etre equele a lui même", money.equals(money));
-        assertFalse("money doit pas etre equela a un null", money.equals(null));
+        assertFalse("Un string n'est pas un objet Money", money.equals("Hello"));
+        assertTrue("Money doit etre egale a lui même", money.equals(money));
+        assertFalse("money ne doit pas etre egale a un null", money.equals(null));
     }
 
     @Test
     public void testToString() {
         Money money = new Money(10, "EUR");
-        assertEquals("Un string n'est a un Money", "Montant: 10\nDevise: EUR", money.toString());
+        assertEquals("Le string n'est pas correct", "Montant: 10\nDevise: EUR", money.toString());
     }
 
 }
