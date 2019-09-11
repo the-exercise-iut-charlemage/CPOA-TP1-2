12f04fd77c4e4a862e80503ac0458d120979df0d
Safyrus <54881649+Safyrus@users.noreply.github.com>
2019-09-11 10:43:08 +0200

    change

--- src/tp2/Annuaire.java
+++ src/tp2/Annuaire.java
@@ -2,5 +2,38 @@ package tp2;
 
 import java.util.HashMap;
 
-public class Annuaire extends HashMap<String, String>{
+public class Annuaire{
+	
+	private HashMap<String, String> map;
+	
+	public static void main(String[] args) {
+		Annuaire a = new Annuaire();
+		a.adjonction("lul", "00");
+		a.adjonction("lol", "00");
+		a.domaine();
+	}
+	
+	public Annuaire() {
+		map = new HashMap<String, String>();
+	}
+	
+	public void domaine() {
+		System.out.println(map.toString());
+	}
+	
+	public String acces(String nom) {
+		return map.get(nom);
+	}
+	
+	public void adjonction(String nom, String num) {
+		map.put(nom, num);
+	}
+	
+	public void suppression(String nom) {
+		map.remove(nom);
+	}
+	
+	public void changement(String nom, String num) {
+		map.replace(nom, num);
+	}
 }
