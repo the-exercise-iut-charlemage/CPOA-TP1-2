e87918244ae4490d74f0574f4df98715f366b35d
liotamiu <42389128+liotamiu@users.noreply.github.com>
2019-09-11 09:19:00 +0200

    exo 2

--- src/tp2/exo2/ListeIp.java
+++ src/tp2/exo2/ListeIp.java
@@ -1,4 +1,13 @@
 package tp2.exo2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ListeIp {
+    private Set<String> listIP;
+
+    public ListeIp() {
+        this.listIP = new HashSet<>();
+    }
+
 }
