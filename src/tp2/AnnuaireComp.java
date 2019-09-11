7a4e48fc75270064107021ceee5b5c9562cce8e4
liotamiu <42389128+liotamiu@users.noreply.github.com>
2019-09-11 08:56:02 +0200

    finsh code of AnnuaireComp

--- src/tp2/AnnuaireComp.java
+++ src/tp2/AnnuaireComp.java
@@ -14,4 +14,19 @@ public class AnnuaireComp extends HashMap<Personne, String>{
         return hashMap.get(new Personne(nom, prenom));
     }
 
+    public void domaine() {
+        this.hashMap.keySet().forEach(e -> System.out.println(e.getNom()));
+    }
+
+    public void adjonction(String nom, String prenom, String num) {
+        this.hashMap.put(new Personne(nom, prenom), num);
+    }
+
+    public void suppression(String nom, String prenom) {
+        this.hashMap.remove(new Personne(nom, prenom));
+    }
+
+    public void changement(String nom, String prenom, String num) {
+        this.hashMap.replace(new Personne(nom, prenom), num);
+    }
 }
