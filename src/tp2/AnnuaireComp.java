945000a0683f6f0bb7c50013b5e29ec1cd6e67f1
liotamiu <42389128+liotamiu@users.noreply.github.com>
2019-09-11 08:42:26 +0200

    Anuaire complex

--- src/tp2/AnnuaireComp.java
+++ src/tp2/AnnuaireComp.java
@@ -5,7 +5,13 @@ import java.util.HashMap;
 public class AnnuaireComp extends HashMap<Personne, String>{
 
     private HashMap<Personne, String> hashMap;
+    
+    public AnnuaireComp() {
+        this.hashMap = new HashMap<>();
+    }
 
-
+    public String acces(String nom, String prenom) {
+        return hashMap.get(new Personne(nom, prenom));
+    }
 
 }
--- src/tp2/Personne.java
+++ src/tp2/Personne.java
@@ -1,5 +1,7 @@
 package tp2;
 
+import java.util.Objects;
+
 public class Personne {
 
     private String nom, prenom;
@@ -23,5 +25,19 @@ public class Personne {
     public Personne(String nom, String prenom) {
         this.nom = nom;
         this.prenom = prenom;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Personne personne = (Personne) o;
+        return nom.equals(personne.nom) &&
+                prenom.equals(personne.prenom);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nom, prenom);
     }
 }
