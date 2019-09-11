14c32bc34909fed0f4ebd2844dfac8c318d16c82
liotamiu <42389128+liotamiu@users.noreply.github.com>
2019-09-11 08:29:39 +0200

    Add code into Personne

--- src/tp2/AnnuaireComp.java
+++ src/tp2/AnnuaireComp.java
@@ -4,4 +4,8 @@ import java.util.HashMap;
 
 public class AnnuaireComp extends HashMap<Personne, String>{
 
+    private HashMap<Personne, String> hashMap;
+
+
+
 }
--- src/tp2/Personne.java
+++ src/tp2/Personne.java
@@ -2,4 +2,26 @@ package tp2;
 
 public class Personne {
 
+    private String nom, prenom;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
 }
