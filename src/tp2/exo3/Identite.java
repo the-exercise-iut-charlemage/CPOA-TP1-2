0b4362d8884f0da928c9ecf6b70d21ca3cff6fc4
Safyrus <54881649+Safyrus@users.noreply.github.com>
2019-09-18 09:28:51 +0200

    add identite

---
+++ src/tp2/exo3/Identite.java
@@ -0,0 +1,16 @@
+package tp2.exo3;
+
+public class Identite {
+
+	private String NIP;
+	private String nom;
+	private String prenom;
+	
+	public Identite(String NIP, String nom, String prenom) {
+		this.NIP = NIP;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	
+}
