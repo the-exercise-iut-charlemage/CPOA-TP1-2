8603228f6c98806afe081a4190de35e1cf9c07e9
liotamiu <42389128+liotamiu@users.noreply.github.com>
2019-09-18 07:41:04 +0200

    update uml & update formation

--- src/tp2/exo3/Formation.java
+++ src/tp2/exo3/Formation.java
@@ -1,14 +1,19 @@
 package tp2.exo3;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Formation {
 
     private List<String> matieres;
 
+    private Map<String, Integer> coefficients;
+
     public Formation() {
         this.matieres = new ArrayList<>();
+        this.coefficients = new HashMap<>();
     }
 
     public void ajouterMatiere(String matiere) {
@@ -17,5 +22,11 @@ public class Formation {
 
     public void supprimerMatiere(String matiere) {
         this.matieres.remove(matiere);
+    }
+
+    public int coefMatier(String matiere) throws IllegalArgumentException {
+        Integer i = this.coefficients.get(matiere);
+        if (i != null) return i;
+        else throw new IllegalArgumentException("La matiere demandé (" + matiere + ") n'est pas presnete dans cette formation!");
     }
 }
